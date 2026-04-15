1f4378a16443087ce450b7070380bbb6a90c4c79
Jason Pell <jason@adnuntius.com>
2026-04-15 18:06:59 +1000

    add a no_reconnect flag for mcus rather than hard coded mcu names

--- klippy/mcu.py
+++ klippy/mcu.py
@@ -831,6 +831,7 @@ class MCU:
         self.is_non_critical = config.getboolean("is_non_critical", False)
         if self.is_non_critical and self.get_name() == "mcu":
             raise error("Primary MCU cannot be marked as non-critical!")
+        self.no_reconnect = config.getboolean("no_reconnect", False)
         if self.is_non_critical:
             self.non_critical_recon_timer = self._reactor.register_timer(
                 self.non_critical_recon_event
@@ -990,6 +991,9 @@ class MCU:
         self.gcode.respond_info(f"mcu: '{self._name}' disconnected!", log=True)
 
     def non_critical_recon_event(self, eventtime):
+        # do not support reconnect for some MCUs which are problematic
+        if self.no_reconnect:
+            return self._reactor.NEVER
         success = self.recon_mcu()
         if success:
             self.gcode.respond_info(
