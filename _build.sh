b7231ec2b8c83ef3fd0cf46f777c66c63b794d9c
Jason Pell <jason@pellcorp.com>
2026-08-08 17:46:00 +1000

    oops forgot to copy the config

--- _build.sh
+++ _build.sh
@@ -8,14 +8,15 @@ if [ "$1" = "host" ]; then
   export CROSS_PREFIX=mips-linux-gnu-
 fi
 
-#cp .config.$1 .config
+cp .config.$1 .config
 mkdir -p outfw/
 make clean
 make
+
 if [ "$1" = "host" ]; then
-  mv out/klipper.elf fw/K1/klipper_host_mcu
+  mv out/klipper.elf outfw/klipper_host_mcu
 elif [ "$1" = "btteddy" ]; then
-  mv out/klipper.uf2 fw/K1/btteddy.uf2
+  mv out/klipper.uf2 outfw/btteddy.uf2
 else
   echo "$1 not supported"
 fi
--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 cd $SCRIPT_DIR
 
+mkdir -p outfw/
+rm -rf outfw/*
+
 ./_build.sh host || exit $?
 ./_build.sh btteddy || exit $?
+
+mv outfw/* fw/K1/
+
