00f285ad88830b5883377914103d93481bb85f9a
sterling5241 <sterling5241@gmail.com>
2026-09-15 14:36:07 -0500

    Update ads131m0x.py

--- klippy/extras/load_cell/ads131m0x.py
+++ klippy/extras/load_cell/ads131m0x.py
@@ -5,7 +5,6 @@
 # ADS131M0x Chip Documentation:
 # ADS131M02 - https://www.ti.com/lit/ds/symlink/ads131m02.pdf
 # ADS131M04 - https://www.ti.com/lit/ds/symlink/ads131m04.pdf
-
 import logging
 from math import floor
 from typing import Dict
