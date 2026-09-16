2a452af4de63e46194a4f048a24922083daa44ec
sterling5241 <sterling5241@gmail.com>
2026-09-16 08:21:44 -0500

    cherry picking

--- klippy/extras/load_cell/hx711s.py
+++ klippy/extras/load_cell/hx711s.py
@@ -4,8 +4,6 @@
 # Original HX711 driver Copyright (C) 2024 Gareth Farrington <gareth@waves.ky>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-from __future__ import annotations
-
 import logging
 
 from klippy.mcu import MCU
